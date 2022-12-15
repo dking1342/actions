f5d098da2f87156c66ce4830dd1d59c4ecdc94e2
Kavooce <kavooce.cheetah@gmail.com>
2022-12-14 21:06:06 -0600

    edit cicd file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -28,10 +28,6 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
   }
 
   provisioner "local-exec" {
-    command = "sudo snap connect doctl:kube-config"
-  }
-
-  provisioner "local-exec" {
     command = "doctl kubernetes cluster kubeconfig save ${digitalocean_kubernetes_cluster.cluster.id}"
   }
 
