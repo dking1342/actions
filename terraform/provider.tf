c98868795f75007a40c84bea797e944f6312ffdb
Kavooce <kavooce.cheetah@gmail.com>
2022-12-15 00:14:49 -0600

    edit cicd file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -23,7 +23,7 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-2vcpu-8gb"
+    size       = "s-4vcpu-8gb"
     node_count = 2
   }
 
